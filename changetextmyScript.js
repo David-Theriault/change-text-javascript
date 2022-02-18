4961e83a3231df43808d92295fe00d3bf686da8b
David Theriault <dayvidca@gmail.com>
2022-02-18 12:58:41 -0500

    Update changetextmyScript.js

--- changetextmyScript.js
+++ changetextmyScript.js
@@ -1,4 +1,4 @@
-// this code changes what is wwritten on a page with a button click
+// this code changes what is wwritten on a page with a button click to whatever is in the " " marks.
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
